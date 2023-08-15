a422caad1e2fca917f77451c4b4a830235774a6f
Michael Owens <mick.owens24@gmail.com>
2023-08-15 03:01:33 -0400

    "Basic start to the App"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-content">
+        <header className="App-header">
+          <div><h1><b>X DATA ADMIN</b></h1></div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div className = "Sidebar">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+          </ul>
+        </div>
+        <div className = "Box-one"><p><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue ut lectus arcu bibendum at. Sit amet mattis vulputate enim nulla aliquet porttitor. Arcu cursus vitae congue mauris rhoncus aenean vel. Semper viverra nam libero justo laoreet sit amet cursus. Turpis egestas sed tempus urna et pharetra. Cras sed felis eget velit aliquet sagittis id consectetur purus. A iaculis at erat pellentesque adipiscing commodo. Blandit libero volutpat sed cras ornare arcu dui vivamus. Id semper risus in hendrerit gravida. Blandit cursus risus at ultrices. Eget aliquet nibh praesent tristique magna sit amet. Dolor sed viverra ipsum nunc aliquet bibendum. Tempus imperdiet nulla malesuada pellentesque elit eget. Eget nunc lobortis mattis aliquam. Turpis egestas pretium aenean pharetra magna ac placerat.
+          </b></p></div>
+        <div className = "Box-two"><p><b>2</b></p></div>
+        <div className = "Box-three"><p><b>3</b></p></div>
+      </main> 
     </div>
   );
 }
+
+
+
+
 
 export default App;
